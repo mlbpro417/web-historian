5d7614a03bbf160700141ad28c92d3f1915f26d8
mlbpro417 <jtglandorf@gmail.com>
2018-02-07 17:14:43 -0800

    finished helper functions

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -26,16 +26,55 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  // read urls from sites.txt
+  // use paths.list
+  // need a callback function to deal with async behavior
+  fs.readFile(exports.paths.list, (err, data) => {
+    callback(err, `${data}`.split('\n'));
+  });
 };
 
+
 exports.isUrlInList = function(url, callback) {
+  // check to see if requested url is in our list
+  // use paths.list
+
+  fs.readFile(exports.paths.list, (err, data) => {
+    if (data.toString().indexOf(url) >= 0) {
+      callback(err, true);
+    } else {
+      callback(err, false);
+    }
+  });
 };
 
 exports.addUrlToList = function(url, callback) {
+  // add requested url to list
+  // use paths.list
+  fs.appendFile(exports.paths.list, url, (err, data) => {
+    callback(err, url);
+  });
 };
 
 exports.isUrlArchived = function(url, callback) {
+  // check to see if the requested url is in archive
+  // use paths.archivedSites
+  
+  fs.readdir(exports.paths.archivedSites, (err, data) => {
+    if (data.indexOf(url) >= 0) {
+      callback(err, true);
+    } else {
+      callback(err, false);
+    }
+  });
 };
 
 exports.downloadUrls = function(urls) {
+  // download to our archive
+  // use siteAssets and archivedSites?
+  // fs.writeFileSync(archive.paths.archivedSites + urls, 'blah blah'
+  urls.forEach(function(url) {
+    fs.writeFileSync(exports.paths.archivedSites + '/' + url);
+  });
+
 };
